433c2fe2147ae6511eddfd9c69bee411f3d33ffa
Aya1113 <122072824+Aya1113@users.noreply.github.com>
2023-02-14 10:29:55 +0900

    Add files via upload

---
+++ データベース中身登録プログラム/csv/Main.java
@@ -0,0 +1,11 @@
+package csv;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		CsvReader c = new CsvReader(); 
+		c.csvWordsReader();
+		c.csvrecipeReader();
+	}
+}
