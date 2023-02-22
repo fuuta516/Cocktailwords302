1f2ac19201b063c71ac06659900a77675969ead4
3030737 <3030737@30307-37>
2023-02-22 10:14:10 +0900

    Merge branch 'master' of https://github.com/fuuta516/Cocktailwords302

--- src/main/java/com/example/demo/dto/ReviewRequest.java
+++ src/main/java/com/example/demo/dto/ReviewRequest.java
@@ -3,6 +3,8 @@ package com.example.demo.dto;
 import java.io.Serializable;
 import java.util.Date;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 /**
@@ -14,7 +16,9 @@ public class ReviewRequest implements Serializable {
   private Long id;
   
   private Date hiduke;
-	
+  
+  @NotEmpty(message = "ニックネームを入力してください")
+  @Size(max = 10, message = "名前は10桁以内で入力してください")
   private String cocktailwordsname;
   
   private String nickname;
@@ -23,7 +27,9 @@ public class ReviewRequest implements Serializable {
   
   private String age;
   
+  @Size(max = 50, message = "コメントは50桁以内で入力してください")
   private String comment;
   
+  @NotEmpty(message = "評価をしてください")
   private Integer evaluation;
 }
